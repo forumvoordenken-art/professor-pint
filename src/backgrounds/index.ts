4aa5433d26119a0dfc33f12ccd97b2ceee926074
forumvoordenken-art <forumvoordenken@gmail.com>
2026-02-12 18:36:05 +0100

    Delete src/backgrounds directory

--- src/backgrounds/index.ts
+++
@@ -1,8 +0,0 @@
-export { Pub } from './Pub';
-export { Classroom } from './Classroom';
-export { Pyramids } from './Pyramids';
-export { DesertConstruction } from './DesertConstruction';
-export { InsidePyramid } from './InsidePyramid';
-export { NileRiver } from './NileRiver';
-export { WorkersVillage } from './WorkersVillage';
-export { SphinxView } from './SphinxView';
