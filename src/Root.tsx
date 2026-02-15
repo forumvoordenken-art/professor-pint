7e39fbba4bc756853763c4983a87e63de56e46b8
forumvoordenken-art <forumvoordenken@gmail.com>
2026-02-15 12:36:41 +0000

    Stage & Actor systeem: Stage.tsx + Scene1.tsx compositie

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,10 +5,21 @@ import { PubInteriorScene, PUB_INTERIOR_FRAMES } from './videos/PubInteriorScene
 import { HistoryOfMoney, HISTORY_OF_MONEY_FRAMES } from './videos/HistoryOfMoney';
 import { SpriteWalkerTest, SPRITE_TEST_FRAMES } from './videos/SpriteWalkerTest';
 import { SVGWalkerTest, SVG_WALKER_TEST_FRAMES } from './videos/SVGWalkerTest';
+import { Scene1, SCENE1_FRAMES } from './compositions/Scene1';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* ── Stage & Actor: Scene 1 ── */}
+      <Composition
+        id="Scene1"
+        component={Scene1}
+        durationInFrames={SCENE1_FRAMES}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+
       {/* ── Main video ── */}
       <Composition
         id="History-of-Money"
